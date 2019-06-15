d5a6338cca1f27180fcde2954c70be2cc7671ac3
Yunato <yunato525@gmail.com>
2019-06-15 17:43:23 +0900

    Connect MainActivity activity to EasyModeFixedActivity activity

--- app/src/main/java/io/github/yunato/myrecordtimer/ui/activity/EasyModeFixedActivity.kt
+++ app/src/main/java/io/github/yunato/myrecordtimer/ui/activity/EasyModeFixedActivity.kt
@@ -1,10 +1,16 @@
 package io.github.yunato.myrecordtimer.ui.activity
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import io.github.yunato.myrecordtimer.R
 
 class EasyModeFixedActivity : AppCompatActivity() {
+
+    companion object {
+        fun intent(context: Context): Intent = Intent(context, EasyModeFixedActivity::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/io/github/yunato/myrecordtimer/ui/fragment/MainFragment.kt
+++ app/src/main/java/io/github/yunato/myrecordtimer/ui/fragment/MainFragment.kt
@@ -1,11 +1,14 @@
 package io.github.yunato.myrecordtimer.ui.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.github.yunato.myrecordtimer.databinding.FragmentMainBinding
+import io.github.yunato.myrecordtimer.ui.activity.EasyModeFixedActivity
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
 
@@ -14,6 +17,15 @@ class MainFragment : Fragment() {
                                savedInstanceState: Bundle?): View? {
         val binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        card_easy_mode_fixed.setOnClickListener{ view ->
+            EasyModeFixedActivity.intent(activity as Context).let{
+                startActivity( it )
+            }
+        }
     }
 
     companion object {
