74d39e5607961b1129c5346e61fda3fe8f34e46e
Yunato <yunato525@gmail.com>
2019-06-15 17:42:31 +0900

    Change fullscreen at EasyModeFixedActivity activity

--- app/src/main/java/io/github/yunato/myrecordtimer/ui/activity/EasyModeFixedActivity.kt
+++ app/src/main/java/io/github/yunato/myrecordtimer/ui/activity/EasyModeFixedActivity.kt
@@ -10,4 +10,18 @@ class EasyModeFixedActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_easy_mode_fixed)
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        val decor: android.view.View = window.decorView
+        decor.systemUiVisibility =
+            android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    android.view.View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    android.view.View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+    }
 }
