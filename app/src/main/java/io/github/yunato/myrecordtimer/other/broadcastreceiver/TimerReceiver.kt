a5752f7f6d028e04029eee8b8cf92f9317eb4489
Yunato <yunato525@gmail.com>
2019-06-17 18:51:41 +0900

    Create TimerReceiver broadcastreceiver

---
+++ app/src/main/java/io/github/yunato/myrecordtimer/other/broadcastreceiver/TimerReceiver.kt
@@ -0,0 +1,13 @@
+package io.github.yunato.myrecordtimer.other.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class TimerReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("TimerReceiver.onReceive() is not implemented")
+    }
+}
