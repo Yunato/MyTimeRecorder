17f48f59ece0f1aadba0cd4680e567caddc83b2e
Yunato <yunato525@gmail.com>
2019-06-17 18:53:40 +0900

    Edit TimerReceiver broadcastreceiver to send string value

--- app/src/main/java/io/github/yunato/myrecordtimer/other/broadcastreceiver/TimerReceiver.kt
+++ app/src/main/java/io/github/yunato/myrecordtimer/other/broadcastreceiver/TimerReceiver.kt
@@ -3,11 +3,34 @@ package io.github.yunato.myrecordtimer.other.broadcastreceiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 
 class TimerReceiver : BroadcastReceiver() {
+    private var handler = Handler()
+    private var isSet = false
 
     override fun onReceive(context: Context, intent: Intent) {
-        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        TODO("TimerReceiver.onReceive() is not implemented")
+        val bundle = intent.extras
+        val timeText = bundle.getString(KEY_TIME_TEXT)
+
+        if(isSet){
+            val msg = Message()
+            val sendBundle = Bundle()
+            sendBundle.putString(KEY_TIME_TEXT, timeText)
+            msg.data = sendBundle
+            handler.sendMessage(msg)
+        }
+    }
+
+    fun registerHandler(handler: Handler) {
+        this.handler = handler
+        isSet = true
+    }
+
+    companion object {
+        const val KEY_TIME_TEXT = "io.github.yunato.myrecordtimer.other.broadcastreceiver.KEY_TIME_TEXT"
     }
 }
+
