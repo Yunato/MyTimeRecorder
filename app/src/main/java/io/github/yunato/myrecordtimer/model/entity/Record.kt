963fc87f30014f95dc1d4bef696fc36acc0974fd
Yunato <yunato525@gmail.com>
2019-06-22 14:43:50 +0900

    Fix Record to be Parcelable object

--- app/src/main/java/io/github/yunato/myrecordtimer/model/entity/Record.kt
+++ app/src/main/java/io/github/yunato/myrecordtimer/model/entity/Record.kt
@@ -1,10 +1,36 @@
 package io.github.yunato.myrecordtimer.model.entity
 
-import java.util.*
+import android.os.Parcel
+import android.os.Parcelable
 
 data class Record(val id: String,
-                  val start: Date,
-                  val end: Date,
+                  val start: Long,
+                  val end: Long,
                   val title: String,
-                  val memo: String,
-                  val eval: Int)
+                  val memo: String?,
+                  val eval: Int) : Parcelable {
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Record> = object : Parcelable.Creator<Record> {
+            override fun createFromParcel(source: Parcel): Record = source.run {
+                Record(readString(), readLong(), readLong(), readString(), readString(), readInt())
+            }
+
+            override fun newArray(size: Int): Array<Record?> = arrayOfNulls(size)
+        }
+    }
+
+    override fun describeContents(): Int = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.run {
+            writeString(id)
+            writeLong(start)
+            writeLong(end)
+            writeString(title)
+            writeString(memo)
+            writeInt(eval)
+        }
+    }
+}
