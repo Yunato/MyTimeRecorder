39e3f6720eeec5a0fb8a5574343b75f03bf9360a
Yunato <yunato525@gmail.com>
2019-06-23 14:47:13 +0900

    Apply factory method pattern to DaoFactory and LocalDao

--- app/src/main/java/io/github/yunato/myrecordtimer/model/dao/DaoFactory.kt
+++ app/src/main/java/io/github/yunato/myrecordtimer/model/dao/DaoFactory.kt
@@ -1,4 +1,10 @@
 package io.github.yunato.myrecordtimer.model.dao
 
+import android.content.Context
+
 class DaoFactory {
+    companion object {
+        @JvmStatic
+        fun getLocalDao(context: Context?): LocalDao = LocalDao.getInstance(context)
+    }
 }
--- app/src/main/java/io/github/yunato/myrecordtimer/model/dao/LocalDao.kt
+++ app/src/main/java/io/github/yunato/myrecordtimer/model/dao/LocalDao.kt
@@ -1,4 +1,51 @@
 package io.github.yunato.myrecordtimer.model.dao
 
-class LocalDao {
+import android.content.Context
+import io.github.yunato.myrecordtimer.model.dao.calendar.LocalCalendarDao
+import io.github.yunato.myrecordtimer.model.dao.event.LocalEventDao
+import io.github.yunato.myrecordtimer.model.entity.Record
+
+class LocalDao private constructor(context: Context){
+
+    private val calendarDao: LocalCalendarDao by lazy{
+        LocalCalendarDao(context)
+    }
+    private val eventDao: LocalEventDao by lazy {
+        LocalEventDao(context)
+    }
+
+    fun createCalendar(){
+        if(!calendarDao.checkExistCalendar()) calendarDao.createCalendar()
+    }
+
+    fun logCalendarInfo(){
+        if(calendarDao.checkExistCalendar()) calendarDao.logCalendarInfo()
+    }
+
+    fun getAllEventItems(): List<Record>{
+        return if(calendarDao.checkExistCalendar()) eventDao.getAllEventItems()
+        else listOf()
+    }
+
+    fun getEventItemsOnDay(year: Int, month: Int, dayOfMonth: Int): List<Record>{
+        return if(calendarDao.checkExistCalendar()) return eventDao.getEventItemsOnDay(year, month, dayOfMonth)
+        else listOf()
+    }
+
+    fun insertEventItem(eventItems: List<Record>): List<String>{
+        return if(calendarDao.checkExistCalendar()) return eventDao.insertEventItems(eventItems)
+        else listOf()
+    }
+
+    fun deleteEventItem(eventId: Long){
+        if(calendarDao.checkExistCalendar()) eventDao.deleteEventItem(eventId)
+    }
+
+    companion object {
+        private var instance: LocalDao? = null
+
+        fun getInstance(context: Context?): LocalDao = instance ?: synchronized(this) {
+            instance ?: LocalDao(context ?: throw IllegalArgumentException("context is null"))
+        }.also { instance = it }
+    }
 }
