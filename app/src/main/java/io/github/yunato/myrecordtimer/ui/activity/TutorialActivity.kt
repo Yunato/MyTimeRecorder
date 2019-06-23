77b7f2df05b38f3dc15ceb17a1a4f0951f01d9a1
Yunato <yunato525@gmail.com>
2019-06-23 17:14:26 +0900

    Create TutorialActivity activity

---
+++ app/src/main/java/io/github/yunato/myrecordtimer/ui/activity/TutorialActivity.kt
@@ -0,0 +1,11 @@
+package io.github.yunato.myrecordtimer.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class TutorialActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
