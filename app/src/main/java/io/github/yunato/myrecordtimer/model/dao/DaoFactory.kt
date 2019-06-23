29fc9248da437a19892bc9a9f3452f592ae02a97
Yunato <yunato525@gmail.com>
2019-06-23 17:04:56 +0900

    Apply factory method pattern to DaoFactory and RemoteDao

--- app/src/main/java/io/github/yunato/myrecordtimer/model/dao/DaoFactory.kt
+++ app/src/main/java/io/github/yunato/myrecordtimer/model/dao/DaoFactory.kt
@@ -6,5 +6,8 @@ class DaoFactory {
     companion object {
         @JvmStatic
         fun getLocalDao(context: Context?): LocalDao = LocalDao.getInstance(context)
+
+        @JvmStatic
+        fun getRemoteDao(context: Context?): RemoteDao = RemoteDao.getInstance(context)
     }
 }
--- app/src/main/java/io/github/yunato/myrecordtimer/model/dao/RemoteDao.kt
+++ app/src/main/java/io/github/yunato/myrecordtimer/model/dao/RemoteDao.kt
@@ -1,4 +1,56 @@
 package io.github.yunato.myrecordtimer.model.dao
 
-class RemoteDao {
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
+import com.google.api.client.util.ExponentialBackOff
+import com.google.api.services.calendar.CalendarScopes
+import io.github.yunato.myrecordtimer.model.dao.calendar.RemoteCalendarDao
+import io.github.yunato.myrecordtimer.model.dao.event.RemoteEventDao
+import io.github.yunato.myrecordtimer.model.entity.Record
+
+class RemoteDao private constructor(context: Context) {
+
+    private val mCredential: GoogleAccountCredential by lazy{
+        val pref = context.getSharedPreferences(DaoPreference.IDENTIFIER_PREF, MODE_PRIVATE)
+        GoogleAccountCredential.usingOAuth2(context.applicationContext, SCOPES)
+            .setBackOff(ExponentialBackOff()).setSelectedAccountName(pref.getString(DaoPreference.PREF_ACCOUNT_NAME, null))
+    }
+    private val calendarDao: RemoteCalendarDao by lazy{
+        RemoteCalendarDao(context, mCredential)
+    }
+    private val eventDao: RemoteEventDao by lazy {
+        RemoteEventDao(context, mCredential)
+    }
+
+    fun createCalendar(){
+        if(!calendarDao.checkExistCalendar()) calendarDao.createCalendar()
+    }
+
+    fun logCalendarInfo(){
+        if(calendarDao.checkExistCalendar()) calendarDao.logCalendarInfo()
+    }
+
+    fun getAllEventItems(): List<Record>{
+        return if(calendarDao.checkExistCalendar()) eventDao.getAllEventItems()
+        else listOf()
+    }
+
+    fun insertEventItem(eventItems: List<Record>): List<String>{
+        return if(calendarDao.checkExistCalendar()) return eventDao.insertEventItems(eventItems)
+        else listOf()
+    }
+
+    fun deleteEventItem(eventId: Long){
+        if(calendarDao.checkExistCalendar()) eventDao.deleteEventItem(eventId)
+    }
+
+    companion object {
+        private var instance: RemoteDao? = null
+        private val SCOPES = mutableListOf(CalendarScopes.CALENDAR)
+
+        fun getInstance(context: Context?): RemoteDao = instance ?: synchronized(this) {
+            instance ?: RemoteDao(context ?: throw IllegalArgumentException("context is null"))
+        }.also { instance = it }
+    }
 }
