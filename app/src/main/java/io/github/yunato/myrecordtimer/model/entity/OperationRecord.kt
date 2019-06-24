bd4145fc9e3ce887a3c02abd8bba5ffe70bb5571
Yunato <yunato525@gmail.com>
2019-06-25 07:39:57 +0900

    Add _id to OperationRecord and RelationRecord

--- app/src/main/java/io/github/yunato/myrecordtimer/model/entity/OperationRecord.kt
+++ app/src/main/java/io/github/yunato/myrecordtimer/model/entity/OperationRecord.kt
@@ -1,4 +1,5 @@
 package io.github.yunato.myrecordtimer.model.entity
 
-data class OperationRecord (val operation: Int,
+data class OperationRecord (val id: Long,
+                            val operation: Int,
                             val calendarId: Long)
--- app/src/main/java/io/github/yunato/myrecordtimer/model/entity/RelationRecord.kt
+++ app/src/main/java/io/github/yunato/myrecordtimer/model/entity/RelationRecord.kt
@@ -1,4 +1,5 @@
 package io.github.yunato.myrecordtimer.model.entity
 
-data class RelationRecord (val localCalendarId: Long,
+data class RelationRecord (val id: Long,
+                           val localCalendarId: Long,
                            val remoteCalendarId: Long)
