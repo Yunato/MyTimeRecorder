00ae8fb8b8a271602511252a4ed8d8cf435911c0
Yunato <yunato525@gmail.com>
2019-06-25 11:28:40 +0900

    Organize ListFragment

--- app/src/main/java/io/github/yunato/myrecordtimer/ui/fragment/ItemFragment.kt
+++ app/src/main/java/io/github/yunato/myrecordtimer/ui/fragment/ItemFragment.kt
@@ -1,6 +1,5 @@
 package io.github.yunato.myrecordtimer.ui.fragment
 
-import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
@@ -10,91 +9,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.github.yunato.myrecordtimer.R
+import io.github.yunato.myrecordtimer.model.dao.calendars.DaoFactory
+import io.github.yunato.myrecordtimer.model.entity.Record
 
-import io.github.yunato.myrecordtimer.ui.fragment.dummy.DummyContent
-import io.github.yunato.myrecordtimer.ui.fragment.dummy.DummyContent.DummyItem
-
-/**
- * A fragment representing a list of Items.
- * Activities containing this fragment MUST implement the
- * [ItemFragment.OnListFragmentInteractionListener] interface.
- */
 class ItemFragment : Fragment() {
 
-    // TODO: Customize parameters
-    private var columnCount = 1
+    private val columnCount = 1
+    private var listener: MyItemRecyclerViewAdapter.OnClickItem? = null
 
-    private var listener: OnListFragmentInteractionListener? = null
+    init{
+        listener = object : MyItemRecyclerViewAdapter.OnClickItem{
+            override fun onClickItem(item: Record) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_item_list, container, false)
 
-        // Set the adapter
         if (view is RecyclerView) {
             with(view) {
-                layoutManager = when {
+                  layoutManager = when {
                     columnCount <= 1 -> LinearLayoutManager(context)
                     else -> GridLayoutManager(context, columnCount)
                 }
-                adapter = MyItemRecyclerViewAdapter(DummyContent.ITEMS, listener)
+                adapter = MyItemRecyclerViewAdapter(DaoFactory.getLocalDao(activity).getAllEventItems(), listener)
             }
         }
         return view
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnListFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson
-     * [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onListFragmentInteraction(item: DummyItem?)
-    }
-
     companion object {
-
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        // TODO: Customize parameter initialization
         @JvmStatic
-        fun newInstance(columnCount: Int) =
-            ItemFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
-                }
-            }
+        fun newInstance() = ItemFragment
     }
 }
--- app/src/main/java/io/github/yunato/myrecordtimer/ui/fragment/MyItemRecyclerViewAdapter.kt
+++ app/src/main/java/io/github/yunato/myrecordtimer/ui/fragment/MyItemRecyclerViewAdapter.kt
@@ -6,31 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import io.github.yunato.myrecordtimer.R
-
-
-import io.github.yunato.myrecordtimer.ui.fragment.ItemFragment.OnListFragmentInteractionListener
-import io.github.yunato.myrecordtimer.ui.fragment.dummy.DummyContent.DummyItem
+import io.github.yunato.myrecordtimer.model.entity.Record
 
 import kotlinx.android.synthetic.main.fragment_item.view.*
 
-/**
- * [RecyclerView.Adapter] that can display a [DummyItem] and makes a call to the
- * specified [OnListFragmentInteractionListener].
- * TODO: Replace the implementation with code for your data type.
- */
-class MyItemRecyclerViewAdapter(
-    private val mValues: List<DummyItem>,
-    private val mListener: OnListFragmentInteractionListener?
-) : RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder>() {
+class MyItemRecyclerViewAdapter(private val mValues: List<Record>,
+                                private val mListener: OnClickItem?)
+    : RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder>() {
 
     private val mOnClickListener: View.OnClickListener
 
     init {
         mOnClickListener = View.OnClickListener { v ->
-            val item = v.tag as DummyItem
-            // Notify the active callbacks interface (the activity, if the fragment is attached to
-            // one) that an item has been selected.
-            mListener?.onListFragmentInteraction(item)
+            val item = v.tag as Record
+            mListener?.onClickItem(item)
         }
     }
 
@@ -42,8 +31,8 @@ class MyItemRecyclerViewAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = mValues[position]
-        holder.mIdView.text = item.id
-        holder.mContentView.text = item.content
+//        holder.mIdView.text = item.id
+//        holder.mContentView.text = item.content
 
         with(holder.mView) {
             tag = item
@@ -51,14 +40,14 @@ class MyItemRecyclerViewAdapter(
         }
     }
 
+    interface OnClickItem {
+        fun onClickItem(item: Record)
+    }
+
     override fun getItemCount(): Int = mValues.size
 
     inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
         val mIdView: TextView = mView.item_number
         val mContentView: TextView = mView.content
-
-        override fun toString(): String {
-            return super.toString() + " '" + mContentView.text + "'"
-        }
     }
 }
