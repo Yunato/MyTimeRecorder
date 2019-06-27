69a4b6848750fa620a5d98b9489c00b138915207
Yunato <yunato525@gmail.com>
2019-06-27 14:00:57 +0900

    Implement DividerItemDecoration to add divider SubEditRecyclerViewAdapter

--- app/src/main/java/io/github/yunato/myrecordtimer/ui/other/DividerItemDecoration.kt
+++ app/src/main/java/io/github/yunato/myrecordtimer/ui/other/DividerItemDecoration.kt
@@ -1,4 +1,46 @@
 package io.github.yunato.myrecordtimer.ui.other
 
-class DividerItemDecoration {
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class DividerItemDecoration(context: Context) : RecyclerView.ItemDecoration() {
+
+    private var mDivider: Drawable?
+
+    init {
+        val typedArray = context.obtainStyledAttributes(ATTRS)
+        mDivider = typedArray.getDrawable(0)
+        typedArray.recycle()
+    }
+
+    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        drawVertical(c, parent)
+    }
+
+    private fun drawVertical(c: Canvas, parent: RecyclerView) {
+        val left = parent.paddingLeft
+        val right = parent.width - parent.paddingRight
+
+        val childCount = parent.childCount
+        for (i in 0 until childCount) {
+            val child = parent.getChildAt(i)
+            val params = child.layoutParams as RecyclerView.LayoutParams
+            val top = child.bottom + params.bottomMargin
+            val bottom = top + (mDivider?.intrinsicHeight ?: 0)
+            mDivider?.setBounds(left, top, right, bottom)
+            mDivider?.draw(c)
+        }
+    }
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        outRect.set(0, 0, 0, mDivider?.intrinsicHeight ?: 0)
+    }
+
+    companion object {
+        private val ATTRS = intArrayOf(android.R.attr.listDivider)
+    }
 }
