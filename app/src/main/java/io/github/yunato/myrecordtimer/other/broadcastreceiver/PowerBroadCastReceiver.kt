539cf159742f20ed502fa34559d9d676a05332ab
Yunato <yunato525@gmail.com>
2019-07-01 23:37:55 +0900

    Create PowerBroadCastReceiver

---
+++ app/src/main/java/io/github/yunato/myrecordtimer/other/broadcastreceiver/PowerBroadCastReceiver.kt
@@ -0,0 +1,13 @@
+package io.github.yunato.myrecordtimer.other.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class PowerBroadCastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("PowerBroadCastReciever.onReceive() is not implemented")
+    }
+}
